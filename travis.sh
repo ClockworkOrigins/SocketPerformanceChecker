f21459ab0dd0fcabe4cda0489818bf90b3e42193
Daniel Bonrath <bonne@clockwork-origins.de>
2016-07-02 15:07:58 +0200

    added travis files

---
+++ travis.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -evx
+env | sort
+
+cd ..
+mkdir build || true
+mkdir build/$SPC_TARGET || true
+cd build/$SPC_TARGET
+cmake -DWITH_M2ETIS=$SPC_M2ETIS -DCMAKE_BUILD_TYPE=$SPC_BUILD_TYPE ../..
+make
