e2cecbfb1c2c20f4a2c9211eff82bd4c57b1a167
Daniel Bonrath <bonne@clockwork-origins.de>
2016-07-10 17:55:47 +0200

    removed travis files

--- travis.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -evx
-env | sort
-
-mkdir build || true
-mkdir build/$SPC_TARGET || true
-cd build/$SPC_TARGET
-cmake -DWITH_M2ETIS=$SPC_M2ETIS -DCMAKE_BUILD_TYPE=$SPC_BUILD_TYPE ../..
-make
